d8e62db8ccc8ebc5cbdb4c7f5eacb861ddf3122d
YYRi9 <mesmgr01@gmail.com>
2023-07-28 02:37:45 +0900

    modify: url 수정

--- src/main/java/com/example/instagram/controller/MemberController.java
+++ src/main/java/com/example/instagram/controller/MemberController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/user")
+@RequestMapping("/api/user")
 public class MemberController {
 
     private final MemberService memberService;
